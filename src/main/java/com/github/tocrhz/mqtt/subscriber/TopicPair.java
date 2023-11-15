59b591c79a9795aaa507f0010d4633e950193340
saveole <xgz.cz@foxmail.com>
2023-11-15 14:20:35 +0800

    set group

--- src/main/java/com/github/tocrhz/mqtt/subscriber/TopicPair.java
+++ src/main/java/com/github/tocrhz/mqtt/subscriber/TopicPair.java
@@ -94,6 +94,14 @@ public class TopicPair {
         return qos;
     }
 
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
     public boolean isMatched(String topic) {
         if (this.pattern != null) {
             return pattern.matcher(topic).matches();
